415fc01de5f3c59bbd2a0176572ca2f326edec17
Luc Verhaegen <libv@skynet.be>
2024-03-07 04:31:11 +0100

    boot0: introduce egon header structs
    The __maybe_unused header printing functions were used in the
    creation of this new code, and can be used in future to help debug
    header changes/issues. So there is no point in making them go away.
    
    Signed-off-by: Luc Verhaegen <libv@skynet.be>

--- sunxi-boot0.c
+++ sunxi-boot0.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * (C) Copyright 2020, Andre Przywara
+ * Copyright (c) 2024 Luc Verhaegen <libv@skynet.be>
  *
  * sunxi-boot0: dump basic information about an Allwinner eGON image
  */
@@ -13,13 +14,89 @@
 
 #include "sunxi-fw.h"
 
-#define CHECKSUM_SEED	0x5f0a6c39
+#define __maybe_unused  __attribute__((unused))
 
-#define BOOT0_CHECKSUM	3
-#define BOOT0_LENGTH	4
-#define BOOT0_DRAM_OFS	14
+struct egon_header {
+	uint32_t jump;
+#define EGON_MAGIC_0 "eGON.BT0"
+#define EGON_MAGIC_1 "eGON.BT1"
+	char magic[8];
+#define EGON_CHECKSUM_SEED 0x5f0a6c39
+	uint32_t checksum;
+#define EGON_FILESIZE_ALIGN 4096
+	uint32_t filesize;
+	uint32_t header_size;
+	char header_version[4];
+	uint32_t return_address;
+	uint32_t run_address;
+	char eGON_version[4];
+	char platform_info[8];
+};
 
+static void __maybe_unused
+egon_header_print(FILE *stream, struct egon_header *header)
+{
+	fprintf(stream, "struct egon_header header[1] = {\n");
+
+	fprintf(stream, "\t.jump = 0x%08X,\n", header->jump);
+	fprintf(stream, "\t.magic = \"%c%c%c%c%c%c%c%c\",\n",
+		header->magic[0], header->magic[1],
+		header->magic[2], header->magic[3],
+		header->magic[4], header->magic[5],
+		header->magic[6], header->magic[7]);
+	fprintf(stream, "\t.checksum = 0x%08X,\n", header->checksum);
+	fprintf(stream, "\t.filesize = 0x%08X, /* %dbytes */\n",
+		header->filesize, header->filesize);
+	fprintf(stream, "\t.header_size = 0x%08X,\n", header->header_size);
+	fprintf(stream, "\t.header_version = \"%c%c%c%c\",\n",
+		header->header_version[0], header->header_version[1],
+		header->header_version[2], header->header_version[3]);
+	fprintf(stream, "\t.return_address = 0x%08X,\n",
+		header->return_address);
+	fprintf(stream, "\t.run_address = 0x%08X,\n", header->run_address);
+	fprintf(stream, "\t.eGON_version = \"%c%c%c%c\",\n",
+		header->eGON_version[0], header->eGON_version[1],
+		header->eGON_version[2], header->eGON_version[3]);
+	fprintf(stream, "\t.platform_info = \"%c%c%c%c%c%c%c%c\",\n",
+		header->platform_info[0], header->platform_info[1],
+		header->platform_info[2], header->platform_info[3],
+		header->platform_info[4], header->platform_info[5],
+		header->platform_info[6], header->platform_info[7]);
+
+	fprintf(stream, "};\n\n");
+}
+
+struct egon_header_secondary {
+	uint32_t header_size;
+	char header_version[4];
 #define EGON_DRAM_PARAM_COUNT 32
+	uint32_t dram_param[EGON_DRAM_PARAM_COUNT];
+	/* ignore the rest of this struct for now. */
+};
+
+static void __maybe_unused
+egon_header_secondary_print(FILE *stream,
+			    struct egon_header_secondary *header)
+{
+	int i;
+
+	fprintf(stream, "struct egon_header header[1] = {\n");
+
+	fprintf(stream, "\t.header_size = 0x%08X,\n", header->header_size);
+	fprintf(stream, "\t.header_version = \"%c%c%c%c\",\n",
+	       header->header_version[0],
+	       header->header_version[1],
+	       header->header_version[2],
+	       header->header_version[3]);
+
+	for (i = 0; i < EGON_DRAM_PARAM_COUNT; i++)
+		fprintf(stream, "\t.dram_param[0x%02X] = 0x%08X,\n",
+		       i, header->dram_param[i]);
+
+	fprintf(stream, "\t/* ... */\n");
+
+	fprintf(stream, "};\n\n");
+}
 
 static void
 dram_param_raw_print(FILE *stream, void *raw)
@@ -34,23 +111,24 @@ dram_param_raw_print(FILE *stream, void *raw)
 
 int output_boot0_info(void *sector, FILE *inf, FILE *stream, bool verbose)
 {
-	const uint32_t *boot0 = sector;
-	uint32_t *buffer, i, chksum = CHECKSUM_SEED;
+	struct egon_header *header = sector;
+	struct egon_header_secondary *secondary;
+	uint32_t *buffer, i, chksum = EGON_CHECKSUM_SEED;
 	void *dram_param;
 	size_t ret;
 
 	if (!verbose) {
-		pseek(inf, boot0[BOOT0_LENGTH] - 512);
-		return (boot0[BOOT0_LENGTH] / 512) - 1;
+		pseek(inf, header->filesize - 512);
+		return (header->filesize / 512) - 1;
 	}
 
-	fprintf(stream, "\tsize: %d bytes\n", boot0[BOOT0_LENGTH]);
+	fprintf(stream, "\tsize: %d bytes\n", header->filesize);
 
-	buffer = malloc(boot0[BOOT0_LENGTH]);
+	buffer = malloc(header->filesize);
 	if (!buffer)
 		return 0;
-	ret = fread(buffer + (512 / 4), 1, boot0[BOOT0_LENGTH] - 512, inf);
-	if (ret < boot0[BOOT0_LENGTH] - 512) {
+	ret = fread(buffer + (512 / 4), 1, header->filesize - 512, inf);
+	if (ret < header->filesize - 512) {
 		fprintf(stream, "\tERROR: image file too small\n");
 		free(buffer);
 
@@ -58,17 +136,18 @@ int output_boot0_info(void *sector, FILE *inf, FILE *stream, bool verbose)
 	}
 	memcpy(buffer, sector, 512);
 
-	for (i = 0; i < boot0[BOOT0_LENGTH] / 4; i++)
+	for (i = 0; i < header->filesize / 4; i++)
 		if (i != 3)
 			chksum += buffer[i];
-	if (chksum == boot0[BOOT0_CHECKSUM])
+	if (chksum == header->checksum)
 		fprintf(stream, "\teGON checksum matches: 0x%08x\n", chksum);
 	else
 		fprintf(stream, "\teGON checksum: 0x%08x, programmed: 0x%08x\n",
-			chksum, boot0[BOOT0_CHECKSUM]);
+			chksum, header->checksum);
 	free(buffer);
 
-	dram_param = (void *) &boot0[BOOT0_DRAM_OFS];
+	secondary = (void *) header + header->header_size;
+	dram_param = secondary->dram_param;
 
 	dram_param_raw_print(stream, dram_param);
 
